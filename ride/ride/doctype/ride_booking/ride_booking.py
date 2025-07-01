0148b8f2de70ca4f01cd03c9c0194097dc1c1c29
sameer bhoir <sameer@8848@gmail.com>
2025-07-01 17:25:30 +0530

    feat: add naming series

--- ride/ride/doctype/ride_booking/ride_booking.py
+++ ride/ride/doctype/ride_booking/ride_booking.py
@@ -1,12 +1,23 @@
 # Copyright (c) 2025, Sanskar Technolab and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from datetime import datetime
+from frappe.model.naming import make_autoname
+
 from frappe.model.document import Document
-from frappe.utils import y
 from . ride_booking_utils import calculate_total_amount
 
 class RideBooking(Document):
 	def before_validate(self):
 		calculate_total_amount(self)
+
+	def autoname(self):
+		if not self.vechicle:
+			frappe.throw("Vehicle field is required for naming.")
+
+		current_year = datetime.now().year
+		prefix = f"{self.vechicle}-{current_year}-"
+		
+		self.name = make_autoname(f"{prefix}.####")
 
